11b5abdcc69af501bc8453afb366b7bcbd61977b
Andrey <spacecmetanka@gmail.com>
2022-09-23 16:09:19 +0300

    matrix-sum & sum-digits solution

--- src/get-email-domain.js
+++ src/get-email-domain.js
@@ -10,10 +10,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For the input 'prettyandsimple@example.com', the output should be 'example.com'
  *
  */
-function getEmailDomain(/* email */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+
+function getEmailDomain(email) {
+
+  // throw new NotImplementedError('Not implemented');
+  
+
+  let dog = email.lastIndexOf('@');
+  return email.slice(dog+1)
 }
+console.log(getEmailDomain('prettyandsimple@example.com'), 'example.com');
+console.log(getEmailDomain('someaddress@yandex.ru'), 'yandex.ru');
+console.log(getEmailDomain('very.unusual.@.unusual.com@usual.com'), 'usual.com');
+console.log(getEmailDomain('admin@mailserver2.ru'), 'mailserver2.ru');
+console.log(getEmailDomain('example-indeed@strange-example.com'), 'strange-example.com');
 
 module.exports = {
   getEmailDomain
